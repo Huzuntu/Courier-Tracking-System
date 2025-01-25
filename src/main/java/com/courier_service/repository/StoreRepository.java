f9d63be9d5714149ed0129caa41531d6ae090186
Umut Tölek <umuttolekk@gmail.com>
2025-01-25 12:23:45 +0300

    Switched to JPA repository

--- src/main/java/com/courier_service/repository/StoreRepository.java
+++ src/main/java/com/courier_service/repository/StoreRepository.java
@@ -1,42 +1,8 @@
 package com.courier_service.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.stereotype.Repository;
-
 import com.courier_service.model.store.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-@Repository
-public class StoreRepository
-{
-    private final List<Store> storeList = new ArrayList<>();
-
-    /**
-     * Checks if a store with the given name already exists in the list.
-     *
-     * @param name The name of the store.
-     * @return true if the store exists, false otherwise.
-     */
-    public boolean existsByName(String name) {
-        return storeList.stream().anyMatch(store -> store.getName().equals(name));
-    }
-
-    /**
-     * Adds a store to the in-memory store list.
-     *
-     * @param store The store to add.
-     */
-    public void add(Store store) {
-        storeList.add(store);
-    }
-
-    /**
-     * Retrieves all stores in the list.
-     *
-     * @return a list of all stores.
-     */
-    public List<Store> findAll() {
-        return new ArrayList<>(storeList);
-    }
+public interface StoreRepository extends JpaRepository<Store, Long> {
+  public boolean existsByName(String name);
 }
